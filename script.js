047fd535b66af02866175432cb432f33fe91a47b
Martina <martina.gabrielli@yahoo.it>
2019-05-22 12:06:34 +0100

    Added functions for cookies

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function activateNightMode(){
     jQuery('#night-mode').addClass('show');
     jQuery('#day-mode').removeClass('show');
+    jQuery('body').addClass('dark');
+    Cookies.set('nightWatch', 'active');
 }
 
 function activateDayMode(){
     jQuery('#day-mode').addClass('show');
     jQuery('#night-mode').removeClass('show');
+    jQuery('body').removeClass('dark');
+    Cookies.set('nightWatch', 'inactive')
 }
 
+function checkCookie(){
+    if(Cookies.get('nightWatch') == 'active'){
+        activateNightMode();
+    } else {
+        activateDayMode();
+    }
+}
+
+checkCookie();
 jQuery('#day-mode').click(activateNightMode);
 jQuery('#night-mode').click(activateDayMode);
+
+
